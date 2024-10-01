475e8942f3f1cca679caf46efba62598978e01ed
Ethan Davey (s5709015) <s5709015@bournemouth.ac.uk>
2024-10-01 15:31:11 +0100

    Initial Message

---
+++ my_first_code.py
@@ -0,0 +1,29 @@
+print ("hello world", end= " ")
+print ("hello second world")
+print (14 + 16)
+name = "alex"
+age = 23
+print (f"Hello {name} you are {age} years old")
+time = 11
+if time >= 12:
+    timing = "afternoon"
+else:
+    timing = "morning"
+print (f"Good {timing} {name}")
+
+print(265+5924)
+print(5022-901)
+print(5*10)
+print(604/4)
+print(3%2)
+print(10%4)
+print(9//3)
+print(8//3)
+print(5**2)
+print(4**3)
+
+a = (6*4)
+b = (5**2)
+c = (4%3)
+result = a + b - c 
+print (f"a + b - c = {result}")
